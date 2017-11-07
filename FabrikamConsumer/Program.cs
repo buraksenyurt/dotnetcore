29d0e3c3be680a44ec7c3abdd46a5a2d0507ed12
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2017-11-07 22:42:03 +0300

    Kafka denemeleri eklendi
    Kafka'nın .Net Core 2.0 ile kullanımına ilişkin deneme kodlar eklendi.

---
+++ FabrikamConsumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace FabrikamConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Consumer Tarafı\n");
+            string brokerEndpoint="localhost:9092";
+            Console.WriteLine("Hangi konu başlığına abone olacaksın?");
+            string topicName=Console.ReadLine();
+
+            var config=new Dictionary<string,object>{
+                {"group.id","FabrikamConsumer"},
+                {"bootstrap.servers",brokerEndpoint},
+            };
+
+            using(var consumer=new Consumer<Null,string>(config,null,new StringDeserializer(Encoding.UTF8)))
+            {
+                consumer.OnMessage+=(o,m)=>{
+                    Console.WriteLine($"Duke Nukem diyor ki: {m.Value}");
+                };
+
+                consumer.Subscribe(new List<string>(){topicName});
+                var isCancelled=false;
+                Console.CancelKeyPress+=(_,e)=>{
+                    e.Cancel=true;
+                    isCancelled=true;
+                };
+                Console.WriteLine("Ctrl-C ile çıkabilirsin");
+                while(!isCancelled)
+                {
+                    consumer.Poll(100);
+                }
+            }
+        }
+    }
+}
---
+++ FabrikamProducer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace FabrikamProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Producer Tarafı\n");
+            string brokerEndpoint="localhost:9092";
+            var quotes=File.ReadAllLines("Quotes.txt");
+            Random random=new Random();
+            Console.WriteLine("Bir topic adı girer misin?");
+            string topicName=Console.ReadLine();
+
+            var config=new Dictionary<string,object>{
+                {"bootstrap.servers",brokerEndpoint}
+            };
+
+            using(var producer= new Producer<Null,string>(config,null,new StringSerializer(Encoding.UTF8)))
+            {
+                for(;;)
+                {                       
+                    string message=quotes[random.Next(1,quotes.Length)-1];
+                    var result=producer.ProduceAsync(topicName,null,message).GetAwaiter().GetResult();
+                    Console.WriteLine($"Partition : {result.Partition} Offset : {result.Offset}\n{message}");                        
+                    System.Threading.Thread.Sleep(10000);
+                }
+            }
+        }
+    }
+}
