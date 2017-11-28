53f12d4668827656d03daaf7cf3021e3ee61bb09
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2017-11-28 14:02:51 +0300

    CORS
    Cross-Origin Resource Sharing konusu ile ilgili örnek eklendi.

---
+++ CORS/Contoso/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Contoso
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseCors(bldr=>bldr
+                .WithOrigins("http://localhost:5000")
+                .AllowCredentials()
+            );
+            app.UseMvc();
+        }
+    }
+}
