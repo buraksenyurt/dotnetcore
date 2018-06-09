a9c017f63f9928f828107270931307c13d1cf1ca
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2018-06-09 21:06:07 +0300

    RabbitMQ samples
    TurboNecati Producer, AfacanMurat Consumer rolünde.

---
+++ rabbitmq/AfacanMurat/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+class Program
+{
+    public static void Main()
+    {
+        var connectionFactory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = connectionFactory.CreateConnection())
+        {
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "commands",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var reader = new EventingBasicConsumer(channel);
+                reader.Received += (m, e) =>
+                {
+                    var body = e.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($"{message}");
+                    Thread.Sleep(500);
+                };
+                channel.BasicConsume(queue: "commands",
+                                     autoAck: true,
+                                     consumer: reader);
+
+                Console.WriteLine("Teşekkürler Necati Amca :)");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ rabbitmq/TurboNecati/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+
+namespace TurboNecati
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = connectionFactory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "commands",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var messages = new List<string>{
+                                "Matematik çalış",
+                                "Haftada bir kitap bitir",
+                                "Felsefeyi tanı",
+                                "Havalar sıcak sokak kapısına bir kap su bırak"
+                            };
+
+                    foreach (var message in messages)
+                    {
+                        var body = Encoding.UTF8.GetBytes(message);
+                        channel.BasicPublish(exchange: "",
+                                         routingKey: "commands",
+                                         basicProperties: null,
+                                         body: body);
+                        Console.WriteLine($"'{message}' gönderildi");
+                        Thread.Sleep(300);
+                    }
+                }
+            }
+
+            Console.WriteLine(" Şimdilik bu kadar. Görüşürüz.");
+            Console.ReadLine();
+        }
+    }
+}
